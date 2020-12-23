d561035089735db9e597c88626e81ba27fe14bb4
Aaron White <aaronsw@umich.edu>
2020-12-24 00:36:03 +0100

    ICS download

---
+++ settings.py
@@ -0,0 +1,41 @@
+# Python dictionary for settings
+
+# Dictionary for matching calendar names to a color
+# -1 is default
+# First number is background, 2nd number is foreground
+userColors = {}
+userColors["work"]      = [30,-1] # turquoise
+userColors["home"]      = [33,-1] # dark blue
+userColors["travel"]    = [124,-1] # green
+userColors["crit"]      = [214,-1] # orange
+userColors["grad"]      = [196,-1] # red
+# userColors["default"] = [223,-1]
+userColors["default"]   = [-1,-1]
+userColors["year"]      = [128,-1]
+userColors["month"]     = [220,-1]
+userColors["focus"]     = [39,-1]
+# userColors["select"]    = [122,0]
+userColors["select"]    = [145,0]
+userColors["emergency"]   = [9,-1]
+userColors["indico"]      = [240,-1]
+
+userColors["dialogFocus"]   = [39,8]
+userColors["dialogBackground"]   = [-1,8]
+
+dayNames = ["mon","tues","weds","thurs","fri","sat","sun"]
+# dayNames = ["Workday"]*7
+monthNames = ["none","jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec","extra"]
+
+calPath = "data"
+mainCategory = "work"
+
+showTextbox = True
+# showTextbox = False
+textboxFrac = 0.25
+
+# general settings
+settings = {}
+
+# ICS calendars may use private keys that should be stored safely somewhere
+# How you do that is up to you
+downloadIcsCalendars = eval(open("privateSettings.py").read())
